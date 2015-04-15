c28d56aa5adaf57c827b4d7e0cd7bd11949b2001
Tony Lim <atomictheorist@gmail.com>
2015-04-15 01:37:04 -0700

    Add unit-testing for parser

--- testParser/main.cpp
+++ testParser/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include "../../Parser/Tagger/converter.h"
+
+void testConverter();
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    testConverter();
     return 0;
 }
 
+/**
+ * Test case for Converter class
+ * @param none 
+ */
+void testConverter() {
+    std::cout << "---   Testing Converter -----\n";
+    try {
+        NLP::Converter converted("This is apple"); // Passed
+
+        converted.getWords();
+
+    } catch (const char* e) {
+        cout << "something went wrong : " << "Converter" << endl;
+    }
+    cout << "-------- End of Converter test case -----\n\n";
+}
--- testParser/testParser.pro
+++ testParser/testParser.pro
@@ -1,10 +1,20 @@
 TEMPLATE = app
 CONFIG += console
+QT     += core sql
 CONFIG -= app_bundle
-CONFIG -= qt
+QMAKE_CXXFLAGS += -std=c++11
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    ../../Parser/Tokenizer/stokenize.cpp \
+    ../../Parser/Tokenizer/token.cpp \
+    ../../Parser/Tagger/word.cpp
 
 include(deployment.pri)
 qtcAddDeployment()
+
+HEADERS += \
+    ../../Parser/Tokenizer/stokenize.h \
+    ../../Parser/Tokenizer/token.h \
+    ../../Parser/Tagger/word.h \
+    ../../Parser/Tagger/converter.h
 
