f6af03109ea9dc32dff87b4bd190a1c7547e57c3
Tony Lim <atomictheorist@gmail.com>
2015-04-16 00:19:23 -0700

    Add more testcases

--- testParser/main.cpp
+++ testParser/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "../../Parser/Tagger/converter.h"
 
-void testConverter();
+void testConverter(string sentence);
 
 using namespace std;
 
 int main()
 {
-    testConverter();
+    testConverter("This is a Bear");
+    testConverter("That is not a building");       /// Bug , comma in dictionary
+    testConverter("Who told you that?");
+//    testConverter("That's what she said.");
     return 0;
 }
 
@@ -15,15 +18,22 @@ int main()
  * Test case for Converter class
  * @param none 
  */
-void testConverter() {
+void testConverter(string sentence) {
     std::cout << "---   Testing Converter -----\n";
     try {
-        NLP::Converter converted("This is bear"); // Passed
 
-        converted.getWords();
+        NLP::Converter converted(sentence); // Passed
+        list<NLP::Word> myParsedWords = converted.getWords();
+        for(NLP::Word wd: myParsedWords)
+            cout << wd << " : " << wd.getRawtypes() << endl;
 
     } catch (const char* e) {
         cout << "something went wrong : " << "Converter" << endl;
     }
     cout << "-------- End of Converter test case -----\n\n";
 }
+
+/** BUG
+  * STokenize may not map all characters, if new character is found, just add
+  * it to the token.h (UNKNOWN Token feature not working)
+  */
