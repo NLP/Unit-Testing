98f8cd2a0f9eeb77bb5c0b4e36d9e54fed4a53d7
JosephPKC <josephpkc@gmail.com>
2015-05-01 17:45:02 -0700

    More test cases

--- test-Parser-Grammatica/main.cpp
+++ test-Parser-Grammatica/main.cpp
@@ -11,29 +11,46 @@ void testParserAndGrammatica(const string& sentence);
 
 int main()
 {
-//    Converter C("Who");
-//    std::vector<Word> test = C.getWords();
-//    std::set<WordType> t = test.begin()->getTypes();
+    Converter C("please");
+    std::vector<Word> test = C.getWords();
+    std::set<WordType> t = test.begin()->getTypes();
 
-//    std::set<WordType>::iterator it = t.begin();
-//    for(std::size_t i = 0; it != t.end(); ++it){
-//        cout << WordStringMap[*it] << endl;
-//    }
+    std::set<WordType>::iterator it = t.begin();
+    for(std::size_t i = 0; it != t.end(); ++it){
+        cout << WordStringMap[*it] << endl;
+    }
 //    testParserAndGrammatica("He walks");
 //    testParserAndGrammatica("I like cake");
 //    testParserAndGrammatica("My name is John");
-    testParserAndGrammatica("Who is you");
+//    testParserAndGrammatica("Who are you");
 //    testParserAndGrammatica("Did you eat"); //Gotta add did, does, do; have, had; am, are, is for aux
-//    testParserAndGrammatica("Is John kick the ball");
-//    testParserAndGrammatica("Why should I give you a sentence"); //Why needs to be its own class.. what is should recognized as?
-//    testParserAndGrammatica("You are putting me on the spot");
-//    testParserAndGrammatica("That was not complicated"); //What type is that, and not?
+//    testParserAndGrammatica("Did John kick the ball");
+//    testParserAndGrammatica("Why can I give a sentence to you"); //Why needs to be its own class.. what is should recognized as?
+//    testParserAndGrammatica("You put me on the spot");
+//    testParserAndGrammatica("That was a complex"); //What type is that, and not?
 //    testParserAndGrammatica("The man sent a letter to his son"); //His needs to be included as a determiner
 //    testParserAndGrammatica("The man sends a letter to a son");
 //    testParserAndGrammatica("Is he die"); //Did?
 //    testParserAndGrammatica("Was he kicked"); //was also should be added as an aux, also what is considered a noun
 //    testParserAndGrammatica("What did John kick to Mark");
-//    testParserAndGrammatica("Are you here");
+//    testParserAndGrammatica("Why do you call it");
+//    testParserAndGrammatica("People have it");
+//    testParserAndGrammatica("There is power in a union");
+//    testParserAndGrammatica("People want things");
+//    testParserAndGrammatica("The speaker conveys his hatred");
+//    testParserAndGrammatica("Please work");
+
+
+    //"The" is considered a verb
+    //"Who" is considered a noun, and a verb
+    //"Is" is considered a noun
+    //"People" is considered a verb
+
+    //"Should" should be considered a verb
+
+    //No infinitives or gerunds yet
+    //No adjectives or adverbs yet
+    //No S-> IP NP NP support
 
     return 0;
 }
@@ -57,6 +74,12 @@ void testParserAndGrammatica(const string& sentence) {
         for(std::size_t i = 0; i < S.size(); ++i){
             cout << "SENTENCE TYPE: " << sentenceLookUp[S[i].getSentenceType()] << endl;
             cout << "#" << i+1 << ": " << S[i] << endl;
+            cout << "LIST: ";
+            std::vector<SyntaxWord> tAll = S[i].getAll();
+            for(std::size_t j = 0; j < tAll.size(); ++j){
+                cout << tAll[j] << "|";
+            }
+            cout << endl;
         }
 
     } catch (const char* e) {
