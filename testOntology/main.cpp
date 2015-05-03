6c2a8be5d740d56002725ecd5fbaec3273510e76
Tony Lim <atomictheorist@gmail.com>
2015-05-02 19:38:03 -0700

    Add test for query generator, and query insertion

--- testOntology/main.cpp
+++ testOntology/main.cpp
@@ -1,22 +1,75 @@
 #include <iostream>
+#include "../../Parser/Tagger/converter.h"
+#include "../../Grammatica/Grammar Structure/Grammar-with-Map/cfgzero.h"
+#include "../../Grammatica/Syntax-Tree/parser.h"
+#include "../../Grammatica/Syntax-Tree/syntaxtree.h"
+#include "../../Grammatica/Grammar Structure/Grammar-with-Map/cfgq.h"
 #include "../../Granular-Extractor/Database/OntologyDatabase.h"
+#include "../../Granular-Extractor/Database/CorpusQueryGenerator.h"
+#include "../../CONFIG/config.h"
 
 using namespace std;
 
-void testOntologyInsertion();
+void testQueryGenerator(Converter& conv, const string& sentence);
+void testOntologyInsertion(const string& query);/// @note PASSED
 
 int main()
 {
-    testOntologyInsertion ();
+    // using one instance is better for efficiency
+    Converter myConverter;
+    string sentence;
+    cout << "Sentence : ";
+    getline(cin, sentence);
+    testQueryGenerator (myConverter, sentence);
+//    testOntologyInsertion ();
     return 0;
 }
 
-void testOntologyInsertion()
+
+
+/**
+ * Test case for QueryGenerator class
+ * @param none
+ */
+void testQueryGenerator(Converter &conv, const string &sentence) {
+    std::cout << "---   Testing QueryGenerator -----\n";
+    try {
+        conv.setString (sentence);
+        Parser P(CFGQ() , conv.getWords ());
+        STvector S = P.parse ();
+        cout << "Number of possible three : " << S.size () << endl;
+        cout << "Proceeds with insertion to database" << endl;
+        /// for now, make sure vector only have 1 SyntaxTree
+        string stringQuery;
+        QueryGenerator myQry;
+        if(!S.empty ()) {
+            stringQuery = myQry.getQueryOf (S[0]);
+        } else {
+            cout << "Sorry, no sensible structure found" << endl;
+            throw unimplemented_exc();
+        }
+        cout << "result : " << stringQuery << endl;
+        /// Phase 2 : Insertion to database
+        cout << " Inserting to database : " << endl;
+        testOntologyInsertion (stringQuery);
+    } catch (const char* e) {
+        cout << "something went wrong : " << "QueryGenerator" << endl;
+    } catch (const exception &e) {
+            cout << e.what() << endl;
+    }
+    cout << "-------- End of QueryGenerator test case -----\n\n";
+}
+
+
+
+ //// @note PASSED
+void testOntologyInsertion(const string &query)
 {
     /// testing query
     OntologyDatabase testOntologyDB;
-    OntologyDatabase testOntologyDB_two;
+//    OntologyDatabase testOntologyDB_two;
     /// Working
 //    testOntologyDB.testInsertionQuery ("DELETE from ontology");
 //    testOntologyDB.testInsertionQuery ("INSERT INTO ontology(sub, pred, obj) VALUES ('They', 'are' , 'teachers')");
+    testOntologyDB.testInsertionQuery (query);
 }
--- testOntology/testOntology.pro
+++ testOntology/testOntology.pro
@@ -4,12 +4,37 @@ QT     += core sql
 CONFIG -= app_bundle
 QMAKE_CXXFLAGS += -std=c++11
 
-SOURCES += main.cpp
+CONFIG += c++11
+QT     += sql
+
+SOURCES += main.cpp \
+    ../../Parser/Tokenizer/stokenize.cpp \
+    ../../Parser/Tokenizer/token.cpp \
+    ../../Parser/Tagger/word.cpp \
+    ../../Grammatica/Syntax-Tree/parser.cpp \
+    ../../Grammatica/Syntax-Tree/syntaxtree.cpp \
+    ../../Grammatica/Syntax-Tree/syntaxword.cpp \
+    ../../Grammatica/grammar.cpp \
+    ../../Grammatica/cfgzero.cpp \
+#    ../../Grammatica/Grammar Structure/Grammar-with-Map/cfgq.cpp \
+    ../../Grammatica/cfgq.cpp
+
+HEADERS += \
+    ../../Parser/Tokenizer/stokenize.h \
+    ../../Parser/Tokenizer/token.h \
+    ../../Parser/Tagger/word.h \
+    ../../Parser/Tagger/converter.h \
+    ../../Grammatica/Syntax-Tree/parser.h \
+    ../../Grammatica/Syntax-Tree/syntaxtree.h \
+    ../../Grammatica/Syntax-Tree/Tree.h \
+    ../../Grammatica/Syntax-Tree/syntaxword.h \
+    ../../Grammatica/grammar.h \
+    ../../Grammatica/cfgzero.h \
+    ../../CONFIG/config.h \
+#    ../../Grammatica/Grammar Structure/Grammar-with-Map/cfgq.h \
+    ../../Grammatica/cfgq.h \
+    ../../Granular-Extractor/Database/CorpusQueryGenerator.h \
+    ../../Granular-Extractor/Database/OntologyDatabase.h
 
 include(deployment.pri)
 qtcAddDeployment()
-
-HEADERS += \
-    ../../Granular-Extractor/Database/OntologyDatabase.h
-    ../../CONFIG/config.h
-
