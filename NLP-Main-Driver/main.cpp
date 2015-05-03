820c6d99486d6cb85fcb2e22ef8a9733757c5116
JosephPKC <josephpkc@gmail.com>
2015-05-02 17:50:24 -0700

    Created Main Driver Project

---
+++ NLP-Main-Driver/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+//Here is where the program is run:
+//1. Accept an input sentence from user
+//2. Tokenize & Parse
+//3. Store into Database /OR/ Extract from Database
+//4. Print Result if Necessary
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
