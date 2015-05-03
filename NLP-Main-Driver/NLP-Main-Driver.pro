ded5acba258b3365f8ff134cc4a076a16ae73b54
JosephPKC <josephpkc@gmail.com>
2015-05-02 18:32:05 -0700

    Created a Main for the Driver

--- NLP-Main-Driver/NLP-Main-Driver.pro
+++ NLP-Main-Driver/NLP-Main-Driver.pro
@@ -1,7 +1,41 @@
+#TEMPLATE = app
+#CONFIG += console
+#CONFIG -= app_bundle
+##CONFIG -= qt
+#CONFIG += c++11
+#QT += sql
 TEMPLATE = app
 CONFIG += console
+QT     += core sql
 CONFIG -= app_bundle
-CONFIG -= qt
+QMAKE_CXXFLAGS += -std=c++11
 
-SOURCES += main.cpp
+CONFIG += c++11
+QT     += sql
+
+SOURCES += main.cpp \
+    ../../Grammatica/Syntax-Tree/parser.cpp \
+    ../../Grammatica/Syntax-Tree/syntaxtree.cpp \
+    ../../Grammatica/Syntax-Tree/syntaxword.cpp \
+#    ../../Grammatica/Grammar Structure/Grammar-with-Map/cfgq.cpp \
+    ../../Grammatica/cfgq.cpp \
+    ../../Parser/Tagger/word.cpp \
+    ../../Parser/Tokenizer/stokenize.cpp \
+    ../../Parser/Tokenizer/token.cpp \
+    ../../Grammatica/cfgzero.cpp \
+    ../../Grammatica/grammar.cpp
+
+HEADERS += \
+    ../../Grammatica/Syntax-Tree/parser.h \
+    ../../Grammatica/Syntax-Tree/syntaxtree.h \
+    ../../Grammatica/Syntax-Tree/syntaxword.h \
+#    ../../Grammatica/Grammar Structure/Grammar-with-Map/cfgq.h \
+    ../../Grammatica/cfgq.h \
+    ../../Parser/Tagger/converter.h \
+    ../../Parser/Tagger/word.h \
+    ../../Parser/Tokenizer/stokenize.h \
+    ../../Parser/Tokenizer/token.h \
+    ../../CONFIG/config.h \
+    ../../Grammatica/cfgzero.h \
+    ../../Grammatica/grammar.h
 
--- NLP-Main-Driver/main.cpp
+++ NLP-Main-Driver/main.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
-
+#include "../../Grammatica/Syntax-Tree/parser.h"
+#include "../../Parser/Tagger/converter.h"
+#include "../../Grammatica/cfgq.h"
+//#include "../../CONFIG/config.h"
 using namespace std;
-
+using namespace NLP;
 //Here is where the program is run:
 //1. Accept an input sentence from user
 //2. Tokenize & Parse
 //3. Store into Database /OR/ Extract from Database
 //4. Print Result if Necessary
-int main()
-{
-    cout << "Hello World!" << endl;
+
+const int MAX = 100;
+int main(){
+    char input[MAX];
+    string sentence;
+    STvector S;
+
+    cout << "Directions:" << endl;
+    cout << "Type in something. (MAX 100 Characters)" << endl;
+    cout << "Don't make any grammar or spelling mistakes" << endl;
+    cout << "Press \"x\" or \"X\" to exit" << endl << endl;
+
+    while(true){
+        cout << "What is on your mind?" << endl;
+        cin.getline(input,MAX);
+        sentence = input;
+
+        if(sentence.compare("x") == 0 || sentence.compare("X") == 0)
+            break;
+
+        try {
+            Parser P(CFGQ(),Converter(sentence).getWords());
+
+            S = P.parse();
+            for(std::size_t i = 0; i < S.size(); ++i){
+                //Do stuff here to eliminate trees
+            }
+            //Do database stuff here
+            //Store or extract
+
+            //The answer here
+
+        } catch (const char* e) {
+            cout << "something went wrong : " << "Main Driver" << endl;
+        }
+    }
+    cout << endl << "Goodbye!" << endl;
     return 0;
 }
 
